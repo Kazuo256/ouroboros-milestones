9eef374b8751210867f46ff002909a82db868998
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-10-22 15:08:27 -0200

    [milestone-05] New exported class data::Info

---
+++ milestone-05/src/info.h
@@ -0,0 +1,30 @@
+
+#ifndef OUROBOROS_MILESTONE_PROMPT_INFO_H_
+#define OUROBOROS_MILESTONE_PROMPT_INFO_H_
+
+namespace data {
+
+class Info {
+
+  public:
+
+    Info ();
+
+    const char* subject ();
+    const char* predicate ();
+    const char* object ();
+
+    void set_subject (const char* the_subject);
+    void set_predicate (const char* the_predicate);
+    void set_object (const char* the_object);
+
+  private:
+
+    char subject_[256], predicate_[256], object_[256];
+
+};
+
+} // namespace data
+
+#endif // OUROBOROS_MILESTONE_PROMPT_INFO_H_
+
